031c51ee9c61e55579e7763afd22e40604ece7e2
Zoro <1329878250@qq.com>
2017-01-06 21:03:30 +0800

    Initial commit

---
+++ app/src/main/java/com/zorolee/android/geoclock/MainActivity.java
@@ -0,0 +1,84 @@
+package com.zorolee.android.geoclock;
+
+import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class MainActivity extends Activity {
+    private TextView positionTextView;
+    private LocationManager locationManager;
+    private String provider;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        positionTextView = (TextView)findViewById(R.id.position_text_view);
+        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        provider = LocationManager.NETWORK_PROVIDER;
+        List<String> providerList = locationManager.getProviders(true);
+        if (providerList.contains(LocationManager.NETWORK_PROVIDER)) {
+            provider = LocationManager.NETWORK_PROVIDER;
+        } else if (providerList.contains(LocationManager.GPS_PROVIDER)) {
+            provider = LocationManager.GPS_PROVIDER;
+        } else {
+            Toast.makeText(this,"No location provider to use",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        try {
+            Location location = locationManager.getLastKnownLocation(provider);
+            if (location != null) {
+                showLocation(location);
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 10,locationListener);
+            }
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+    private void showLocation(Location location) {
+        String latitude = "" + location.getLatitude();
+        String longitude = "" + location.getLongitude();
+        positionTextView.setText("latitude is " + latitude + "\n" + "longitude is " + longitude);
+
+    }
+
+    LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            showLocation(location);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        try {
+            if (locationManager != null) {
+                locationManager.removeUpdates(locationListener);
+            }
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+}
